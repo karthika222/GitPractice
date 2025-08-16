6b2b8c4df8bf473fdbf11dfe0285fcf20d2791d1
karthika222 <krishnakarthika64@gmail.com>
2025-08-16 19:24:57 +0530

    updated

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-import turtle
+'''import turtle
 
 print("hello")
 
@@ -6,4 +6,15 @@ turtle.circle(45)   # turn left 45 degrees
 #turtle.forward(100)  # move forward 100 pixels
 turtle.triangle(30)
 
+turtle.done()  # keep the window open'''
+import turtle
+
+print("hello")
+
+# Draw a triangle with side length 100
+for _ in range(3):
+    turtle.forward(100)
+    turtle.right(120)  # turn left 120° to make a triangle
+
 turtle.done()  # keep the window open
+
